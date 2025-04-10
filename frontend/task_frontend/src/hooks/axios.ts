638e357df74d226f30d3b41e47a8889ef8eae8d4
Sanjay Karki <karkisanjay2002@gmail.com>
2025-04-10 22:36:27 +0545

    env changes

--- frontend/task_frontend/src/hooks/axios.ts
+++ frontend/task_frontend/src/hooks/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-const BASE_URL = 'http://localhost:3500'
+// const BASE_URL = 'http://localhost:3500'
 
-//const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 console.log(BASE_URL);
 
